26d24c3bf6f4d3a16016b73d6fc6c61401969303
yuweiZhan <195812208@qq.com>
2020-12-26 22:18:20 +0800

    :sparkles: 83 创建表，从文件读取数据

--- src/main/java/com/example/apitest/table/TableTest2CommonApi.java
+++ src/main/java/com/example/apitest/table/TableTest2CommonApi.java
@@ -2,10 +2,17 @@ package com.example.apitest.table;
 
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.api.java.BatchTableEnvironment;
 import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.table.descriptors.Csv;
+import org.apache.flink.table.descriptors.FileSystem;
+import org.apache.flink.table.descriptors.Schema;
+import org.apache.flink.table.types.DataType;
+import org.apache.flink.types.Row;
 
 /**
  * Desription:
@@ -17,12 +24,13 @@ import org.apache.flink.table.api.java.StreamTableEnvironment;
  **/
 public class TableTest2CommonApi {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         // 创建环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
-
+        // 创建表环境
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
 
         // 基于老版本planner的流处理
         EnvironmentSettings oldStreamSettings = EnvironmentSettings.newInstance()
@@ -50,8 +58,19 @@ public class TableTest2CommonApi {
                 .build();
         TableEnvironment blinkBatchTableEnv = TableEnvironment.create(blinkBatchSettings);
 
+        String filePath = "/Users/yuwei1/Documents/java/project/studing-flink/src/main/resources/sensor.txt";
+        tableEnv.connect(new FileSystem().path(filePath))
+                .withFormat( new Csv())
+                .withSchema( new Schema()
+                        .field("id", DataTypes.STRING())
+                .field("timestamp",DataTypes.BIGINT())
+                .field("temp",DataTypes.DOUBLE()))
+                .createTemporaryTable("inputTable");
 
 
+        Table inputTable = tableEnv.from("inputTable");
+        tableEnv.toAppendStream(inputTable, Row.class).print();
+        env.execute();
 
     }
 }
